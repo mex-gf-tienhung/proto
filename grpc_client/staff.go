bd8160b984338e26216f749f1a3f861b7e3b28a5
Nguyễn Cao Hiệp <apolohwork@gmail.com>
2024-06-04 10:48:02 +0700

    create postion for super admin when add new workspace

---
+++ grpc_client/staff.go
@@ -0,0 +1,56 @@
+package grpc_client
+
+import (
+	"sync"
+
+	"github.com/mex-gf-tienhung/proto/golang/staff"
+	"google.golang.org/grpc"
+)
+
+var (
+	_staffClient        *staffClientStruct
+	loadstaffClientOnce sync.Once
+)
+
+func ConnectTostaffServer(addr string, options ...grpc.DialOption) error {
+	var err error
+	loadstaffClientOnce.Do(func() {
+		_staffClient = new(staffClientStruct)
+		err = _staffClient.Connect(addr, options...)
+	})
+
+	return err
+}
+
+func staffClient() *staffClientStruct {
+	if _staffClient == nil {
+		panic("grpc staff client: like client is not initiated")
+	}
+
+	return _staffClient
+}
+
+type staffClientStruct struct {
+	staff.StaffServiceClient
+	clientConn *grpc.ClientConn
+}
+
+func (c *staffClientStruct) Connect(addr string, options ...grpc.DialOption) error {
+	authConn, err := grpc.Dial(addr, options...)
+	if err != nil {
+		return err
+	}
+
+	c.StaffServiceClient = staff.NewStaffServiceClient(authConn)
+	c.clientConn = new(grpc.ClientConn)
+	c.clientConn = authConn
+	return nil
+}
+
+func (c *staffClientStruct) Close() {
+	if c.clientConn == nil {
+		return
+	}
+
+	defer c.clientConn.Close()
+}
